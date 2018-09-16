da16dcdad4b9bba9805fef4139e75e6abf0e1268
feihu <378917466@qq.com>
2018-09-16 08:29:15 +0800

    Struct2Test

---
+++ src/actions/HelloWordAction.java
@@ -0,0 +1,11 @@
+package actions;
+
+public class HelloWordAction {
+
+    public String execute(){
+
+        System.out.println("I am HelloWordAction.java");
+        return "success";
+    }
+
+}
